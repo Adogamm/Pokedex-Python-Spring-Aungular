a1bfece66fe256455f701d07aae898a5eed994be
Adolfo León <1718110396@utectulancingo.edu.mx>
2023-07-23 01:21:35 -0600

    Creacón del proyecto de angular

---
+++ Front/Pokedex/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
